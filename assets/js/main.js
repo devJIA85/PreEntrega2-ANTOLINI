a959796484a04a48c713a3e633af9bce566a0a73
Juan Ignacio A <j.antolini@me.com>
2023-03-30 11:24:38 -0300

    v 0.0.7
    responsive about section
    js incompleto, no funciona

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,14 +38,52 @@ if(navClose) {
 
 
 /*=============== INPUT ANIMATION ===============*/
+const inputs = document.querySelectorAll(" input");
 
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+function blurFunc() {
+    let parent = this.parentNode;
+    if(this .value == "") {
+        parent.classList.remove("focus");
+    }
+}
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurfunc);
+})
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
-/*=============== SHOW SCROLL UP ===============*/
 // toma todas sections que tienen un id definido
-const sections = document.querySelectorAll("section[id]")
+const sections = document.querySelectorAll("section[id]");
 
 // agrega un event listener para el scroll
 window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter()
+{
+    //get current scroll position
+    let scrollY = window.pageYOffset;
+    //loop through sections para tener height, Top e ID values paracada una
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute("id");
+        /* - If our current scroll position enters the space where current section on screen is, add active class to corresponding navigation link, else remove it
+        - To know which Link needs an active class, we use sectionId variable we are getting while looping through sections as an selector */
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+        {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link")
+        }
+        else
+        {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link")
+        }
+        })
+}
+
+/*=============== SHOW SCROLL UP ===============*/
+
+
