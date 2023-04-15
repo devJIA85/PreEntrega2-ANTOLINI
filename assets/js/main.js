29f1f146bdf3bdbe8887413450646b65b0dcfcdb
Juan Ignacio A <j.antolini@me.com>
2023-04-15 00:17:20 -0300

    v 1.3.4
    Section WRK completa + SCSS y js

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,9 +42,24 @@ const   tabs = document.querySelectorAll('[data-target]'),
         })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-/* buscar!
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll('.work__item');
+
+function activeWork() {
+  linkWork.forEach(l => l.classList.remove('active-work'));
+  this.classList.add('active-work');
+}
+
+linkWork.forEach(l => l.addEventListener("click", activeWork));
 
 
 /*===== Work Popup =====*/
